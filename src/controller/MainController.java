b5b67313ee0dd70e79ee545f12dd90ec43571150
nobu <algebra.zetafunc@gmail.com>
2020-04-29 10:13:48 +0900

    メモ帳起動

---
+++ src/controller/MainController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable  {
+    /** ************************************************************
+     * 初期化処理
+     * 画面表示時に行いたい処理を実装する。
+     * @param location
+     * @param resources
+     * *************************************************************/
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // 何もしない
+    }
+
+    @FXML
+    public void onCreateNewFile() {
+    }
+}
