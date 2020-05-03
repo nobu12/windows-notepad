d0867a32e38dd699456ea35db0e801d68fe14bf0
nobu <algebra.zetafunc@gmail.com>
2020-05-04 00:52:41 +0900

    検索ダイアログのラジオボタン

--- src/controller/SearchNextController.java
+++ src/controller/SearchNextController.java
@@ -8,8 +8,10 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class SearchNextController implements Initializable {
 
@@ -18,6 +20,13 @@ public class SearchNextController implements Initializable {
 
 	@FXML
 	private CheckBox caseSensitive;
+
+	@FXML
+	private ToggleGroup radioButtonGroup;
+	@FXML
+	private RadioButton upSearchRadioButton;
+	@FXML
+	private RadioButton downSearchRadioButton;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -34,7 +43,13 @@ public class SearchNextController implements Initializable {
 		if (textArea.getSelectedText().indexOf(searchText.getText()) == -1) {
 			anchor = textArea.getAnchor();
 		}
+
 		int start = textArea.getText().indexOf(searchText.getText(), anchor);
+		if (upSearchRadioButton == radioButtonGroup.getSelectedToggle()) {
+			anchor = textArea.getAnchor() - 1;
+			start = textArea.getText().lastIndexOf(searchText.getText(), anchor);
+		}
+
 		int end = start + searchText.getText().length();
 		textArea.selectRange(start, end);
 	}
