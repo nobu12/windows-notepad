6abfb8a97c27c8e2039b5ebf0ed0fab2a64b332e
nobu <algebra.zetafunc@gmail.com>
2020-05-17 11:13:19 +0900

    先頭要素だけ

--- src/controller/FaqController.java
+++ src/controller/FaqController.java
@@ -15,6 +15,36 @@ public class FaqController implements Initializable {
     private VBox horizontalScrollAnswer;
 	@FXML
     private Hyperlink fontStyleAndSize;
+	@FXML
+	private VBox fontStyleAndSizeAnswer;
+	@FXML
+	private Hyperlink textEdit;
+	@FXML
+	private VBox textEditAnswer;
+	@FXML
+	private Hyperlink saveFileExtended;
+	@FXML
+	private VBox saveFileExtendedAnswer;
+	@FXML
+	private Hyperlink printDocment;
+	@FXML
+	private VBox printDocmentAnswer;
+	@FXML
+	private Hyperlink insertTimeAndDate;
+	@FXML
+	private VBox insertTimeAndDateAnswer;
+	@FXML
+	private Hyperlink replacementWord;
+	@FXML
+	private VBox replacementWordAnswer;
+	@FXML
+	private Hyperlink createHeaderAndFotter;
+	@FXML
+	private VBox createHeaderAndFotterAnswer;
+	@FXML
+	private Hyperlink moveSpecificLine;
+	@FXML
+	private VBox moveSpecificLineAnswer;
 
     private static FaqController editMenuController;
 
@@ -28,12 +58,19 @@ public class FaqController implements Initializable {
 	}
 
 	/**
-	 * 元に戻す
+	 * 「水平方向にスクロールせずにすべての文字列を表示」リンク押下
 	 */
 	@FXML
 	public void onClickHorizontalScrollLink(Event e) {
 		horizontalScrollAnswer.setVisible(true);
 		fontStyleAndSize.setTranslateY(0);
+		textEdit.setTranslateY(textEdit.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		saveFileExtended.setTranslateY(saveFileExtended.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		printDocment.setTranslateY(printDocment.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		insertTimeAndDate.setTranslateY(insertTimeAndDate.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		replacementWord.setTranslateY(replacementWord.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		createHeaderAndFotter.setTranslateY(createHeaderAndFotter.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
+		moveSpecificLine.setTranslateY(moveSpecificLine.getTranslateY() + horizontalScrollAnswer.getPrefHeight());
 	}
 
 	@FXML
